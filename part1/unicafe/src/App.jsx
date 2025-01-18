0962c6286ba899007721d826041ca37efb055ee6
Jose134 <jmjuradobujalance@gmail.com>
2025-01-18 18:01:58 +0100

    Exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,23 +2,41 @@ import { useState } from 'react'
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const getTotal = () => good + neutral + bad;
+  
+  const getAvg = () => {
+    const total = getTotal();
+    return total == 0 ? 0 : (good - bad) / getTotal();
+  };
+
+  const getPositive = () => {
+    const total = getTotal();
+    return total == 0 ? 0 : (good / getTotal()) * 100;
+  }
 
   return (
     <div>
       <div>
         <h1>Give Feedback</h1>
+
         <button onClick={() => setGood(good + 1)}>good</button>
         <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
         <button onClick={() => setBad(bad + 1)}>bad</button>
       </div>
       <div>
         <h1>Statistics</h1>
+
         <p>Good {good}</p>
         <p>Neutral {neutral}</p>
         <p>Bad {bad}</p>
+
+        <p>Total {getTotal()}</p>
+        <p>Average {getAvg()}</p>
+        <p>Positive {getPositive()} %</p>
       </div>
     </div>
   )
