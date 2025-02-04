e6ced19e38eeaa0dc3ac69b3c3b288d2accde741
Jose134 <jmjuradobujalance@gmail.com>
2025-02-04 18:29:24 +0100

    Exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,27 +1,31 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+    { name: 'Arto Hellas', number: '39-44-5323523' }
+  ]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
-  const handleNewNameChange = (event) => {
-    setNewName(event.target.value);
+  const handleFieldChange = (setterMethod) => {
+    return (event) => {
+      setterMethod(event.target.value);
+    }
   }
 
   const handleAddClick = (event) => {
     event.preventDefault();
     const name = newName.trim();
-    if (name === '') {
-      alert('Name cannot be empty');
+    if (name === '' || newNumber === '') {
+      alert('Name and number cannot be empty');
     }
     else if (persons.some(person => person.name === name)) {
       alert(`${name} is already added to phonebook`);
     }
     else {
       setNewName('');
-      setPersons(persons.concat({ name: name }));
+      setNewNumber('');
+      setPersons(persons.concat({ name: name, number: newNumber }));
     }
   }
 
@@ -30,16 +34,19 @@ const App = () => {
       <h1>Phonebook</h1>
       <form>
         <div>
-          name: <input value={newName} onChange={handleNewNameChange} type='text' />
+          name: <input value={newName} onChange={handleFieldChange(setNewName)} type='text' />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleFieldChange(setNewNumber)} type='text' />
         </div>
         <div>
           <button onClick={handleAddClick} type="submit">add</button>
         </div>
       </form>
       <h1>Numbers</h1>
-      {persons.map(person => (<p key={person.name}>{person.name}</p>))}
+      {persons.map(person => (<p key={person.name}>{person.name}&nbsp;{person.number}</p>))}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
