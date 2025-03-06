f1225b6f82120436f1d5cc0a2cd11562ee7bf76d
Jose134 <jmjuradobujalance@gmail.com>
2025-03-06 19:42:36 +0100

    Exercise 3.8

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -5,28 +5,41 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(bodyParser.json());
-app.use(morgan('tiny'));
+
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+const logger = morgan((tokens, req, res) =>
+    [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
+        tokens['body'](req, res)
+    ].join(' ')
+);
+
+app.use(logger);
 
 const persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+    {
+        "id": "1",
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+    {
+        "id": "2",
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+    {
+        "id": "3",
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
     },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+    {
+        "id": "4",
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
     }
 ]
 
